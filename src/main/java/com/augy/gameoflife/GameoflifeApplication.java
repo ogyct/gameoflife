f490d78a53b442fb067fc19d49d57db4358e693c
Dmitry Avgustis <dmitry.avgustis@zentity.com>
2021-01-25 12:26:09 +0100

    optimized imports

--- src/main/java/com/augy/gameoflife/GameoflifeApplication.java
+++ src/main/java/com/augy/gameoflife/GameoflifeApplication.java
@@ -1,9 +1,6 @@
 package com.augy.gameoflife;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import lombok.SneakyThrows;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
